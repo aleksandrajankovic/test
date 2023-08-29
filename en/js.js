20cbef6e5b20fa04a084fd3ae7552f0c748b9500
aleksandrajankovic <aleksandra.bgd.87@gmail.com>
2023-08-29 19:59:32 +0200

    Update js.js

--- en/js.js
+++ en/js.js
@@ -129,24 +129,7 @@ var swiper = new Swiper(".mySwiper1", {
   },
 });
 
-// Kreiranje MobileDetect objekta
-var md = new MobileDetect(window.navigator.userAgent);
 
-// Funkcija za izvršavanje redirekcije
-function redirectToApp() {
-  var currentUrl = window.location.href; // Dobijanje trenutnog URL-a
-  if (md.is("iPhone") || md.is("iPad") || md.is("iPod")) {
-    window.location.href = "https://itunes.apple.com/gb/app/id1140116001";
-  } else if (md.is("AndroidOS")) {
-    window.location.href =
-      "https://coupons.joker.co.rs/~android/rs/app-rs-release.apk";
-  } else if (md.is("Huawei")) {
-    window.location.href = "https://appgallery.huawei.com/#/app/C104641235";
-  } else {
-    // Ako uređaj nije prepoznat, redirektuj na osnovnu LP
-    currentUrl;
-  }
-}
 
 // Pozivanje funkcije za izvršavanje redirekcije
 redirectToApp();
